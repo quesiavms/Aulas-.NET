730b237440d12e612b1b0e70c114ba3691fd810b
Quesia Silva <quesiavirginia2806@gmail.com>
2025-01-13 19:14:20 -0300

    Meu primeiro commit

--- Aula42/obj/Debug/net8.0/Aula42.AssemblyInfo.cs
+++ Aula42/obj/Debug/net8.0/Aula42.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula42")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7183176dfd2e80e0ff72186c2632b134b783216")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula42")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula42")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
